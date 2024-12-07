140bc2e9a304e5c07fe2fbe47e633e43bda43980
JesusSantiago31 <jesussantiagocr31@gmail.com>
2024-12-07 08:30:00 -0600

    Variables

---
+++ TESJI/Semana1/2Variables.php
@@ -0,0 +1,27 @@
+<?php
+//VARIABLES
+//los nombres de las variables comienzan con el caracter $ y son sensibles a mayusculas y minusculas
+$dia1 = 15;
+$dia2 = 30;
+echo "El valor de la primera variale es: ";
+echo $dia1;
+echo "<br>";
+echo "El valor de la segunda variable es: ";
+echo $dia2;
+
+$edad = 20;
+$peso = 90.5;
+$nombre = "Juan";
+$existe = true;
+echo "Variable integer: ";
+echo $edad;
+echo "<br>";
+echo "Variable double: ";
+echo $peso;
+echo "<br>";
+echo "Variable String: ";
+echo $nombre;
+echo "<br>";
+echo "Variable boolean: ";
+echo $existe;
+?>
