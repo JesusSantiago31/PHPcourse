c8d227c242a72496be9d3bcb00050ed98bd2b596
JesusSantiago31 <jesussantiagocr31@gmail.com>
2024-12-09 08:20:00 -0600

    IMC

---
+++ TESJI/Semana1/10IMC.php
@@ -0,0 +1,17 @@
+<html>
+<head>
+    <title> Problema </title>
+</head>
+<body>
+    <form action="ejercicio10_1.php" method = "post">
+        Ingrese Peso
+        <input type="text" name="peso">
+        <br>
+        Ingrese Estatura
+        <input type="text" name="estatura">
+        <br>
+        
+        <input type="submit" name="operar"> 
+    </form>
+</body>
+</html>
