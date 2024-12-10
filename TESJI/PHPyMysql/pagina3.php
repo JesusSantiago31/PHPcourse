ba30cdcd62e4d1fed283575bfac122732c4dc833
JesusSantiago31 <jesussantiagocr31@gmail.com>
2024-12-09 20:30:00 -0600

    Practica 3

---
+++ TESJI/PHPyMysql/pagina3.php
@@ -0,0 +1,14 @@
+<html> 
+<head> 
+<title>Problema</title> 
+</head> 
+<body> 
+<form action="pagina4.php" method="post"> 
+Ingrese nombre del curso: 
+<br> 
+
+</body> 
+<input type="text" name="nombrecurso" size="40"> 
+<input type="submit" value="Confirmar"> 
+</form> 
+</html> 
