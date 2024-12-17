a1da137f2868a7623dfd9f2de412d229a3d0fc00
JesusSantiago31 <jesussantiagocr31@gmail.com>
2024-12-17 16:25:48 -0600

    Interfaz Resultado Registrar

--- TESJI/PracticaFinal/informacionP.php
+++ TESJI/PracticaFinal/informacionP.php
@@ -2,9 +2,19 @@
  
 <head> 
   <title>Informacion</title> 
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+  <link rel="stylesheet" href="style.css">
 </head> 
  
 <body> 
+<br><br>
+    <div>
+        <h3>REGISTRO</h3>
+    </div>
+    <br>
+    <br>
+    <div>
+
  <?php
 // Se cre la conexión con nustra base de datos
 $conexion = mysqli_connect("localhost", "root", "", "proyecto")
@@ -31,6 +41,11 @@ echo "El Familiar fue dado de alta :)";
 
 
 ?>
+ </div>
+    <br><br>
+    <div>
+    <a href= "index.html" type="button" class="btn btn-outline-info btn-lg"> Menú Principal </a>
+</div>
 </body> 
  
  </html> 
