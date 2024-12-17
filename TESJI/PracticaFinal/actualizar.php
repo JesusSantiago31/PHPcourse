5383c94171dd50c394145127a049cdc243b24a04
JesusSantiago31 <jesussantiagocr31@gmail.com>
2024-12-17 16:17:23 -0600

    Actualizar Interfaz

---
+++ TESJI/PracticaFinal/actualizar.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=<device-width>, initial-scale=1.0">
+    <title>Eliminar</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div>
+        <p class="fs-1 fw-bold"> ACTUALIZAR REGISTRO FAMILIAR </p>
+    </div>
+    <div class="content">
+    <form method="post" action="actualizar2.php" enctype="multipart/form-data">
+    
+        <div class="container px-4">
+    
+            <div class="mb-3" >
+                <h3>ID: </h3>
+                <input type="number"class="form-control" placeholder="Introduce el id a Actualizar" name="id" required>
+            </div>
+        </div>
+
+        <div>
+            <button type="submit" class="btn btn-outline-primary btn-lg" name="registrar"> Registrar </button>
+            <a href= "index.html" type="button" class="btn btn-outline-info btn-lg"> Menú Principal </a>
+        </div>
+    </form>
+    </div>
+
+</body>
+</html>
