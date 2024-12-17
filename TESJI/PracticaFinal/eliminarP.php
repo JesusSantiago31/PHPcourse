64c9e17086768d08c4a01e6a0cfea4398e49838d
JesusSantiago31 <jesussantiagocr31@gmail.com>
2024-12-17 16:22:51 -0600

    Interfaz Resultado Eliminar

--- TESJI/PracticaFinal/eliminarP.php
+++ TESJI/PracticaFinal/eliminarP.php
@@ -4,10 +4,18 @@
 <meta charset="UTF-8">
 <meta name="viewport" content="width=<device-width>, initial-scale=1.0">
   <title>Eliminar</title> 
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
   <link rel="stylesheet" href="style.css">
 </head> 
  
 <body> 
+<br><br>
+    <div>
+        <h3>ELIMINACIÓN</h3>
+    </div>
+    <br>
+    <br>
+    <div>
 <?php
 $conexion = mysqli_connect("localhost", "root", "", "proyecto")
 or
@@ -23,6 +31,12 @@ or
 
 echo("Se eliminó correctamente");
 ?>
+ </div>
+ 
+ <br><br>
+ <div>
+ <a href= "index.html" type="button" class="btn btn-outline-info btn-lg"> Menú Principal </a>
+</div>
 </body> 
  
  </html> 
